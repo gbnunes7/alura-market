c5bb762fdac726fcc0c2dff02ed9c7e80ca81e52
Gabriel Melo <bielrj170@gmail.com>
2024-08-26 13:46:01 -0300

    refactor: refactor style for itemLayout

--- src/itemLayout.js
+++ src/itemLayout.js
@@ -1,17 +1,20 @@
-export const itemLayout = (nome,date) => {
-	const html = `<div class="flex flex-col">
-    <div class="flex text-[#131730] text-base gap-2">
-        <input type="checkbox">
-        <p>${nome}</p>
-        <button>Excluir</button>
-        <button>Editar</button>
-    </div>
-    <div>
-        <p class="text-xs text-[#131730]">${date}</p>
-    </div>
-</div>`;
+export const itemLayout = (nome, date) => {
+	const html = 
+    `<div class="flex flex-col mb-4">
+        <div class="flex text-[#131730] text-base gap-2 justify-between">
+            <input type="checkbox">
+            <p>${nome}</p>
+        <div class= "flex" >
+            <img src='/public/img/delete.svg'></button>
+            <img src='/public/img/edit.svg'></button>
+        </div>
+        </div>
+        <div>
+            <p class="text-xs text-[#131730]">${date}</p>
+        </div>
+    </div>`;
 
 	return html;
 };
 
-export default itemLayout
+export default itemLayout;
